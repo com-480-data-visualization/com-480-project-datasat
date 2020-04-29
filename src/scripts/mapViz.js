3de41c161f77c86079e9963a859dc22357c1d34e
Alexis DEWAELE <alexis.dewaele@epfl.ch>
2020-04-29 16:46:13 +0200

    Fixes Navbar (and adds image). Imports Bootstrap. Adds Hover tool. Limits zoom and pan. Add dropdown filters (don't do anything yet)

--- src/scripts/mapViz.js
+++ src/scripts/mapViz.js
@@ -1,16 +1,97 @@
-var geojson = new L.GeoJSON.AJAX("../data/countries.geojson");
+const defaultStyle = {
+    fillColor: "#F6C101",
+    fillOpacity: .6,
+    weight: 2,
+    color: "#604000",
+    };
+
+function style(feature) {
+  return defaultStyle;
+}
+
+
+
+var geojson = new L.GeoJSON.AJAX("../data/countries.geojson", {style:style, onEachFeature:onEachFeature});
 
 L.mapbox.accessToken = 'pk.eyJ1IjoibG92aXRhbmEiLCJhIjoiY2s5aGM4MWU2MGFmZDNubW5hZzhvYzUwcSJ9.fxbhUaa-uvN57kEkyKAALA';
 var map = L.mapbox.map('map')
     .setView([37.8, -20], 3)
     .addLayer(L.mapbox.styleLayer('mapbox://styles/lovitana/ck9hcar40128e1in25eib56gd'));
-
+map.options.zoomDelta = 0.5;
+map.options.minZoom = 2.5;
+map.options.maxZoom = 7;
+map.setMaxBounds(map.getBounds());
+map.options.maxBoundsViscosity = 1.0;
 //L.geoJson(geojson).addTo(map);
-
 geojson.on('data:loaded', function(){
 geojson.addTo(map);
 });
+/*
+var info = L.control();
 
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+        '<b>' + props.ADMIN + '</b><br />'
+        : 'Hover over a state');
+};
+
+info.addTo(map);
+*/
+function highlightFeature(e, popupContent) {
+  var layer = e.target;
+  layer.setStyle({
+    weight: 5,
+    fillColor: "#F6C101",
+    fillOpacity: 1,
+  });
+
+  //var mouseX = window.event.clientX;
+  //var mouseY = window.event.clientY;
+  //d3.select("#hovertool").style("left", mouseX + "px").style("top", mouseY + 'px').style("opacity", .5);
+/*
+  var mouseX = window.event.clientX;
+  var mouseY = window.event.clientY;
+  var hovertool = document.getElementById("hovertool");
+  hovertool.style.opacity=1;
+  hovertool.style.left = mouseX + 'px';
+  hovertool.style.top = mouseY + 'px';
+*/
+
+}
+function mouseOut(e) {
+  var layer = e.target;
+  layer.setStyle(defaultStyle);
+  map.closePopup();
+  //info.update();
+}
+
+function onMouseClick(e) {
+  
+}
+
+function onEachFeature(feature, layer) {
+  var popupContent = "Top rated beers in " + feature.properties.ADMIN + " :<br> - Feld <br> - Heineken"//feature.properties.ADMIN;
+  /*layer.on({
+    mouseover: highlightFeature,
+    mousemove: function(e) {
+      var popup  = L.popup({closeButton:false}).setLatLng(e.latlng).setContent(popupContent).openOn(map);
+    },
+    mouseout: mouseOut
+  }, {passive:true}); */
+  layer.addEventListener("mouseover", highlightFeature, {passive:true});
+  layer.addEventListener("mousemove", function(e) {
+    //highlightFeature(e);
+    var popup  = L.popup({closeButton:false}).setLatLng(e.latlng).setContent(popupContent).openOn(map);
+
+  }, {passive:true});
+  layer.addEventListener("mouseout", mouseOut, {passive:true});
+}
 
 /*
  NOT USED RN
