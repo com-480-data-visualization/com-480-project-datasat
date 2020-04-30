93321d2e378e64cc9f70efe4fa2ab5f987f10610
Alexis DEWAELE <alexis.dewaele@epfl.ch>
2020-04-30 16:47:53 +0200

    Fixes Map bounds. Adds panel to the right to display informations. Now it displays only basic information based on the selection menus.

--- src/scripts/mapViz.js
+++ src/scripts/mapViz.js
@@ -20,7 +20,7 @@ var map = L.mapbox.map('map')
 //map.options.zoomDelta = 0.5;
 map.options.minZoom = 2.3;
 map.options.maxZoom = 7;
-map.setMaxBounds([-58.9, -175.7], [75.9, 180]);
+map.setMaxBounds(new L.LatLngBounds([-58.9, -175.7], [75.9, 180]));
 map.options.maxBoundsViscosity = 1.0;
 //L.geoJson(geojson).addTo(map);
 geojson.on('data:loaded', function(){
@@ -72,7 +72,7 @@ function mouseOut(e) {
 }
 
 function onMouseClick(e) {
-  
+
 }
 
 function onEachFeature(feature, layer) {
